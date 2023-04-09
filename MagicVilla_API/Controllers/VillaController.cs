ac1fe1d026dc5601e56ef1e5d68615472bfb37a4
Javier18121998 <Javier.Marin.Garcia.Linkedin01@hotmail.com>
2023-04-08 22:24:28 -0600

    Creacion Proyecto

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MagicVilla_API.Models;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> 
+            {
+                new Villa{Id=1, Nombre="Vista a la Piscina"},
+                new Villa{Id=2, Nombre="Vista a la Playa"}
+            };
+        }
+    }
+}
