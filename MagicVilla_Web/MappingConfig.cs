e51b15ed30621b0e2805d92bb7c7fb356a9e5e8e
Javier18121998 <Javier.Marin.Garcia.Linkedin01@hotmail.com>
2023-04-22 21:36:57 -0600

    update in 22 april 2023

---
+++ MagicVilla_Web/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models.DTOs;
+
+namespace MagicVilla_Web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDto>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDto>().ReverseMap();
+            CreateMap<NumeroVillaDto, NumeroVillaCreateDto>().ReverseMap();
+            CreateMap<NumeroVillaDto, NumeroVillaUpdateDto>().ReverseMap();
+        }
+    }
+}
