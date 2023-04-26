ea295a0918691bdef174ea12531876d2c96c1d5f
Javier18121998 <Javier.Marin.Garcia.Linkedin01@hotmail.com>
2023-04-26 17:44:23 -0600

    uodated in 26 april 2023

---
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using MagicVilla_Web.Services.IServices;
+using MagicVilla_Web.Models;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> villaList = new ();
+            var response = await _villaService.ObtenerTodos<APIResponse>();
+            if(response != null && response.IsExitoso){
+                villaList = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Resultado));
+            }
+            return View(villaList);
+        }
+    }
+}
---
+++ MagicVilla_Web/Services/IServices/IVillaService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MagicVilla_Web.Models.DTOs;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface IVillaService
+    {
+        Task<T> ObtenerTodos<T>();
+        Task<T> Obtener<T>(int id);
+        Task<T> Crear<T>(VillaCreateDto dto);
+        Task<T> Actualizar<T>(VillaUpdateDto dto);
+        Task<T> Remover<T>(int id);
+    }
+}
